2754cd03438577bbeb3ea8f9fe662aae95b43511
ChaiyoKung <kananek45@gmail.com>
2024-11-20 21:59:24 +0700

    refactor: restructure app entry point and update start scripts

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get("/api", (req, res) => {
   return res.send("Hello Chaiyo!");
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+module.exports = app;
